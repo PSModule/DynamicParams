b9bb3f8323c7c10a2e6d822c697657832fa87900
Marius Storhaug <marstor@hotmail.com>
2024-03-15 07:57:10 +0100

    fix

--- tests/DynamicParam.Tests.ps1
+++ tests/DynamicParam.Tests.ps1
@@ -1,11 +1,11 @@
-﻿Describe 'DynamicParams' {
+﻿Describe 'DynamicParam' {
     Context 'Module' {
         It 'The module should be available' {
-            Get-Module -Name 'DynamicParams' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'DynamicParams' -ListAvailable | Out-String) -Verbose
+            Get-Module -Name 'DynamicParam' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'DynamicParam' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be importable' {
-            { Import-Module -Name 'DynamicParams' } | Should -Not -Throw
+            { Import-Module -Name 'DynamicParam' } | Should -Not -Throw
         }
     }
 }
