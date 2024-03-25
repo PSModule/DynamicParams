532c39e81cb2a461977d1852117e74ce61b3b6c7
Marius Storhaug <marstor@hotmail.com>
2024-03-25 23:45:11 +0100

    fix

---
+++ examples/DynamicParam2.ps1
@@ -0,0 +1,43 @@
+﻿function Test-DynParam {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [ValidateSet('A', 'B', 'C')]
+        [string]$Param1
+    )
+    DynamicParam {
+        $DynamicParamDictionary = New-DynamicParamDictionary
+
+        $dynParam = @{
+            Name                   = 'Process'
+            Alias                  = 'proc'
+            Type                   = [string]
+            ValidateSet            = Get-Process -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name -Unique
+            DynamicParamDictionary = $DynamicParamDictionary
+        }
+        New-DynamicParam @dynParam
+
+        $dynParam2 = @{
+            Name                   = 'Service'
+            Alias                  = 'svc'
+            Type                   = [string]
+            ValidateSet            = Get-Service -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name -Unique
+            DynamicParamDictionary = $DynamicParamDictionary
+        }
+        New-DynamicParam @dynParam2
+
+        return $DynamicParamDictionary
+    }
+
+    process {
+        $PSBoundParameters.Keys | ForEach-Object {
+            Set-Variable -Name $_ -Value $PSBoundParameters[$_]
+        }
+
+        Write-Host "Param1: $Param1"
+        Write-Host "Process: $Process"
+        Write-Host "Service: $Service"
+    }
+}
+
+Test-DynParam -Param1 A -Service SDRSVC
