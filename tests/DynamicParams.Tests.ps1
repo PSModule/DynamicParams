5ace4a0bb150f30c0b30b0e8a441472473b2a46a
Marius Storhaug <marstor@hotmail.com>
2024-03-28 22:14:52 +0100

    align

--- tests/DynamicParams.Tests.ps1
+++ tests/DynamicParams.Tests.ps1
@@ -1,11 +1,18 @@
-﻿Describe 'DynamicParams' {
+﻿[CmdletBinding()]
+Param(
+    # Path to the module to test.
+    [Parameter()]
+    [string] $Path
+)
+
+Describe 'DynamicParams' {
     Context 'Module' {
         It 'The module should be available' {
             Get-Module -Name 'DynamicParams' -ListAvailable | Should -Not -BeNullOrEmpty
             Write-Verbose (Get-Module -Name 'DynamicParams' -ListAvailable | Out-String) -Verbose
         }
         It 'The module should be importable' {
-            { Import-Module -Name 'DynamicParams' -Verbose -Force } | Should -Not -Throw
+            { Import-Module -Name 'DynamicParams' -Verbose -RequiredVersion 999.0.0 -Force } | Should -Not -Throw
         }
     }
 }
