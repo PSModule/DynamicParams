b7e949582c862aa6b8c95ea2f94c21a4c7807986
Marius Storhaug <marstor@hotmail.com>
2024-11-01 23:55:23 +0100

    🩹 [Patch]: Remove redundant parameter from tests (#17)
    ## Description
    
    - Remove redundant parameter from tests
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/DynamicParams.Tests.ps1
+++ tests/DynamicParams.Tests.ps1
@@ -1,13 +1,4 @@
-﻿[CmdletBinding()]
-Param(
-    # Path to the module to test.
-    [Parameter()]
-    [string] $Path
-)
-
-Write-Verbose "Path to the module: [$Path]" -Verbose
-
-Describe 'DynamicParams' {
+﻿Describe 'DynamicParams' {
     Context 'Function: New-DynamicParamDictionary' {
         It 'New-DynamicParamDictionary should not throw an exception' {
             { New-DynamicParamDictionary } | Should -Not -Throw
